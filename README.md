7df6d0b047db26c7a63bbbe92ce48c025dfc98b3
Bob Stammers <stammers.bob@gmail.com>
2021-01-31 16:58:13 +0000

    Initial beta

---
+++ README.md
@@ -0,0 +1,43 @@
+# OLD VOICE RECORDINGS
+
+This provides standalone access to a static collection of voice recordings from the now defunct
+BT voice recorder currently in Runcton.
+
+I am my own web server accessing my own SQLite database and voice recordings stored in MP3 format
+on disk. Recordings themselves are separate files in \[a single\] disk folder but they're indexed
+by date and phone number in the database.
+
+This is not intended to be a changeable dataset.
+
+## Database contents
+
+### Table **cdrs**
+This contains one record for each Call Detail Record as follows:-
+|   Field   |  Type    | Contents         |
+|-----------|----------|------------------|
+| cdrid     | string   | unique reference |
+| direction | boolean  | in/out           |
+| duration  | timespan | hh:mm:ss         |
+| connected | datetime | ISO8601          |
+| aphone    | string   | calling number   |
+| bphone    | string   | called number    |
+| folderid  | integer  | data path ix     |
+| dvdname*  | string   | archive name     |
+
+These records are extracted and reformatted as necessary from original BT database.
+
+\* dvdname is the label of the DVD which should also contain this record. DVDs are not actually used
+by this system at all.
+
+### Table **folders**
+This contains one record for each data volume folder managed by the system.
+|   Field   |  Type    | Contents         |
+|-----------|----------|------------------|
+| folderid  | integer  | unique reference |
+| datapath  | string   | full path        |
+
+### Table **params**
+This contains one record for each data volume folder managed by the system.
+|   Field   |  Type    | Contents                     |
+|-----------|----------|------------------------------|
+| dbname    | string   | description of this database |
---
+++ main.go
@@ -0,0 +1,308 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/pkg/browser"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	debug    = flag.Bool("debug", false, "enable debugging")
+	port     = flag.String("port", "80", "the HTTP port")
+	db3      = flag.String("db", "cdrs.db", "full path to db")
+	starturl = flag.String("url", "http://localhost", "starting URL")
+	pagesz   = flag.Int("pg", 15, "Pagesize for results")
+)
+
+const myversion = "v0.1"
+
+var sqldb *sql.DB
+
+func fetchTemplate(template string) string {
+
+	file, err := os.Open(template)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	b, err := ioutil.ReadAll(file)
+	return string(b)
+}
+
+func getValueFromDB(sql, colname, defvalue string) string {
+
+	row, err := sqldb.Query(sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	var res string = defvalue
+	if row.Next() {
+		var rex string
+		row.Scan(&rex)
+		res = rex
+	}
+	return res
+
+}
+
+func showDuration(duration string) string {
+
+	var re = regexp.MustCompile(`\:`)
+	var res string = ""
+	hms := re.Split(duration, 3)
+	h, _ := strconv.Atoi(hms[0])
+	m, _ := strconv.Atoi(hms[1])
+	s, _ := strconv.Atoi(hms[2])
+	if h != 0 {
+		res += strconv.Itoa(h) + "h "
+		res += strconv.Itoa(m) + "m "
+	} else if m != 0 {
+		res += strconv.Itoa(m) + "m "
+	}
+	if s != 0 {
+		res += strconv.Itoa(s) + "s "
+	}
+	return res
+}
+
+func showDate(dt string) string {
+
+	layout := "2006-01-02"
+	t, _ := time.Parse(layout, dt)
+
+	return t.Format("2 Jan 2006")
+}
+
+func showDatetime(dt string) string {
+
+	layout := "2006-01-02T15:04:05Z"
+	t, _ := time.Parse(layout, dt)
+
+	return t.Format("2 Jan 2006 3:04pm Mon")
+}
+
+func countrows(table, where string) int {
+
+	var sql = "SELECT Count(*) As rex FROM " + table + " WHERE " + where
+	var res int
+	res, _ = strconv.Atoi(getValueFromDB(sql, "rex", "-1"))
+	return res
+
+}
+
+func lookupHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	fmt.Fprintf(w, fetchTemplate("htmlhead.html"))
+	fmt.Fprintf(w, "<h2>%v</h2>", getValueFromDB("SELECT dbname FROM params", "dbname", ""))
+	fmt.Fprintf(w, fetchTemplate("htmllookup.html"))
+
+	var tel string = r.FormValue("tel")
+	var daterange bool = r.FormValue("dates") != "all"
+	var fromdate string = r.FormValue(("fromdate"))
+	var todate string = r.FormValue("todate")
+	var offset int = 0
+
+	offset, _ = strconv.Atoi(r.FormValue("offset"))
+
+	fmt.Fprintf(w, "<p>Showing ")
+	if tel != "" {
+		fmt.Fprintf(w, "%v", tel)
+	} else {
+		fmt.Fprintf(w, "all calls")
+	}
+	if daterange {
+		fmt.Fprintf(w, "; Call date ")
+		if fromdate == "" {
+			fmt.Fprintf(w, "upto "+showDate(todate))
+		} else {
+			fmt.Fprintf(w, showDate(fromdate))
+			if todate != "" {
+				if todate != fromdate {
+					fmt.Fprintf(w, " - "+showDate(todate))
+				}
+			} else {
+				fmt.Fprintf(w, " onwards")
+			}
+		}
+	}
+
+	if todate != "" {
+		todate += "T23:59:59"
+	}
+
+	var sql string = "SELECT cdrid,direction,duration,connected,ifnull(aphone,'') as aphone,ifnull(bphone,'') as bphone "
+	var where string = ""
+	sql += "FROM cdrs "
+	sql += "WHERE "
+	if tel != "" {
+		where += "(aphone LIKE '%" + tel + "%' OR bphone LIKE '%" + tel + "%') "
+	} else {
+		where += "1=1 "
+	}
+	if daterange {
+		if fromdate != "" {
+			where += " AND connected >= '" + fromdate + "'"
+		}
+		if todate != "" {
+			where += " AND connected <= '" + todate + "'"
+		}
+	}
+	nresults := countrows("cdrs", where)
+
+	fmt.Fprintf(w, "; %v found</p>", nresults)
+
+	sql += where
+	sql += " LIMIT " + strconv.Itoa(offset) + ", " + strconv.Itoa(*pagesz)
+
+	fmt.Fprintf(w, "\n<!-- %v -->\n", sql)
+
+	rows, err := sqldb.Query(sql)
+	if err != nil {
+		fmt.Fprintf(w, "OMG!!! %v", err)
+		return
+	}
+	fmt.Fprintf(w, "<table id=\"results\"><thead><tr>")
+	fmt.Fprintf(w, "<th class=\"duration\">Duration</th>")
+	fmt.Fprintf(w, "<th class=\"connected\">Connected</th>")
+	fmt.Fprintf(w, "<th class=\"direction\">I/O</th>")
+	fmt.Fprintf(w, "<th class=\"aphone\">From</th>")
+	fmt.Fprintf(w, "<th class=\"bphone\">To</th></tr></thead><tbody>")
+	for rows.Next() {
+		var cdrid string
+		var direction string
+		var duration string
+		var connected string
+		var aphone, bphone string
+		rows.Scan(&cdrid, &direction, &duration, &connected, &aphone, &bphone)
+		var record string = "/cdr/{" + cdrid + "}.osf"
+		fmt.Fprintf(w, "<tr><td class=\"duration\">%v</td>", showDuration(duration))
+		fmt.Fprintf(w, "<td class=\"connected\">%v</td>", showDatetime(connected))
+		fmt.Fprintf(w, "<td class=\"direction\">%v</td>", direction)
+		fmt.Fprintf(w, "<td class=\"aphone\">%v</td><td class=\"bphone\">%v</td>", aphone, bphone)
+		fmt.Fprintf(w, "<td class=\"audio\"><audio controls><source src=")
+		fmt.Fprintf(w, "\"%v\" type=\"audio/mpeg\"></audio></td>", record)
+		fmt.Fprintf(w, "</tr>")
+	}
+	fmt.Fprintf(w, "</tbody></table>")
+
+	if offset > 0 {
+		fmt.Fprintf(w, "<form action=\"lookup\" method=\"post\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"tel\" value=\""+r.FormValue("tel")+"\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"dates\" value=\""+r.FormValue("dates")+"\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"fromdate\" value=\""+r.FormValue("fromdate")+"\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"todate\" value=\""+r.FormValue("todate")+"\">")
+		var poffset int = 0
+		if offset >= *pagesz {
+			poffset = offset - *pagesz
+		}
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"offset\" value=\""+strconv.Itoa(poffset)+"\">")
+		fmt.Fprintf(w, "<input type=\"submit\" value=\"&NestedLessLess;\"> ")
+		fmt.Fprintf(w, "</form>")
+
+	}
+
+	if nresults-offset > *pagesz {
+		fmt.Fprintf(w, "<form action=\"lookup\" method=\"post\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"tel\" value=\""+r.FormValue("tel")+"\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"dates\" value=\""+r.FormValue("dates")+"\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"fromdate\" value=\""+r.FormValue("fromdate")+"\">")
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"todate\" value=\""+r.FormValue("todate")+"\">")
+		var poffset int = offset + *pagesz
+		fmt.Fprintf(w, "<input type=\"hidden\" name=\"offset\" value=\""+strconv.Itoa(poffset)+"\">")
+		fmt.Fprintf(w, "<input type=\"submit\" value=\"&NestedGreaterGreater;\"> ")
+		fmt.Fprintf(w, "</form>")
+
+	}
+
+}
+
+func startServer() {
+
+	var numrex int
+	row, err := sqldb.Query("SELECT Count(*) As rex FROM cdrs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if row.Next() {
+		row.Scan(&numrex)
+		fmt.Printf("Number of CDRs: %v\n", numrex)
+	}
+	row.Close()
+	browser.OpenURL(*starturl + ":" + *port)
+
+}
+
+func handleFolder(folderid int, datapath string) {
+
+	cdrserver := http.FileServer(http.Dir(datapath))
+	cdrf := "/cdr" + strconv.Itoa(folderid) + "/"
+	http.Handle(cdrf, http.StripPrefix(cdrf, cdrserver))
+
+}
+
+func handleFolders() {
+
+	rows, err := sqldb.Query("SELECT folderid,datapath FROM folders")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var folderid int
+		var datapath string
+		rows.Scan(&folderid, &datapath)
+		handleFolder(folderid, datapath)
+	}
+
+}
+
+func main() {
+
+	fmt.Printf("OldVRs %v\nCopyright (c) Bob Stammers 2021\n\n", myversion)
+
+	flag.Parse()
+
+	var err error
+	sqldb, err = sql.Open("sqlite3", *db3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqldb.Close()
+
+	fmt.Printf("Database: %v\n", getValueFromDB("SELECT dbname FROM params", "dbname", "unidentified"))
+	fileServer := http.FileServer(http.Dir("."))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/lookup", lookupHandler)
+
+	handleFolders()
+
+	go startServer()
+
+	fmt.Printf("Serving port " + *port + "\n")
+	if err := http.ListenAndServe(":"+*port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
